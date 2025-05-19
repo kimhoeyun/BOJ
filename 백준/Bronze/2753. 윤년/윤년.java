d4e0075f84d3d2c669ede14865a6d7c50f110d8d
kimhoeyun <clnvrd@suwon.ac.kr>
2025-05-19 20:50:41 +0900

    [Bronze V] Title: 윤년, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -6,11 +6,13 @@ public class Main {
 
         int y = scanner.nextInt();
 
-        if(y % 4 == 0 && y % 100 != 0) {
-            System.out.println("1");
-        }
-        else if(y % 4 == 0 && y % 400 == 0) {
-            System.out.println("1");
+        if(y % 4 == 0) {
+            if(y % 100 != 0 || y % 400 == 0) {
+                System.out.println("1");
+            }
+            else {
+                System.out.println("0");
+            }
         }
         else {
             System.out.println("0");

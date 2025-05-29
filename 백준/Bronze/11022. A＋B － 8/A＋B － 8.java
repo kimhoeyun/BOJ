2ef8c1e3c439f405c8507648b880df2dc3939ca4
kimhoeyun <clnvrd@suwon.ac.kr>
2025-05-29 20:11:12 +0900

    [Bronze V] Title: A+B - 8, Time: 152 ms, Memory: 16416 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < t; i++) {
+            String[] arr = br.readLine().split(" "); 
+
+            int a = Integer.parseInt(arr[0]);
+            int b = Integer.parseInt(arr[1]);
+
+            int s = a + b;
+            
+            bw.write("Case #" + (i + 1) + ": " + a + " + " + b + " = " + s + "\n");
+        }
+        bw.flush();
+    }
+}
